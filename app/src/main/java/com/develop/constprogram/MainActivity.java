1825efb4e3cd1188a7681c9db48c6f03cd42993a
Amos <amosdorceus2010@gmail.com>
2020-06-24 14:29:42 -0400

    Amos' Change (Dashboard listview, listprogram, ProgramAdapter,ProgramModel)

--- app/src/main/java/com/develop/constprogram/MainActivity.java
+++ app/src/main/java/com/develop/constprogram/MainActivity.java
@@ -1,13 +1,45 @@
 package com.develop.constprogram;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+Button btn, btnListDashboard;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up_first);
+        setContentView(R.layout.activity_main);
+
+        btn=findViewById(R.id.toListProgram);
+        btnListDashboard=findViewById(R.id.btnListDashboard);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,Recycle.class);
+                startActivity(intent);
+            }
+        });
+
+        btnListDashboard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,Dashboard.class);
+                startActivity(intent);
+            }
+        });
+        /*list= (ListView) findViewById(R.id.list_program);
+        adapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1,values);
+        // Assign adapter to ListView
+        list.setAdapter(adapter);*/
     }
 }
